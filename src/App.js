646402de9d04de0e7da85ba78a09db3172f5d054
DEV_YUN <riosee2415@gmail.com>
2019-11-12 01:53:27 +0900

    multi return -- used 'Fragment'

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
-import React from "react";
+import React, { Component, Fragment } from "react";
 
-class App extends React.Component {
+class ReturnTypes extends Component {
   render() {
-    return <div>Hello</div>;
+    return (
+      <Fragment>
+        <div>Fragment : div-1</div>
+        <div>Fragment : div-2</div>
+      </Fragment>
+    );
+  }
+}
+
+class ReturnTypesString extends Component {
+  render() {
+    return "Just String";
+  }
+}
+
+class App extends Component {
+  render() {
+    return (
+      <Fragment>
+        <ReturnTypes />
+        <ReturnTypesString />
+      </Fragment>
+    );
   }
 }
 
